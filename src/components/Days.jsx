a451c37a7138f7e8465f0a298428dbdc02275c72
alphamuhia <muhiaalpha@gmail.com>
2024-10-30 12:54:36 +0300

    done

--- src/components/Days.jsx
+++ src/components/Days.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function Days() {
-    const days_list = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  const days_list = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
   return (
     <div>
-        <h2>Days of the Week</h2>
-        <ul>
-            {days_list.map((day, index)=>(
-                <li key={index}>{day}</li>
-            ))}
-        </ul>
+      <h2>Days of the Week</h2>
+      <h3>Call function name</h3>
+      <ul>
+        {days_list.map((day, index) => (
+          <li key={index}>{day}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Days
+export default Days;
